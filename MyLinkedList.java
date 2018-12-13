be42786b709abeec8b89231b2a4968802bd0c262
Kayla Fang <kfang00@stuy.edu>
2018-12-13 14:50:39 -0500

    added code for size()

--- MyLinkedList.java
+++ MyLinkedList.java
@@ -1,13 +1,21 @@
 public class MyLinkedList{
- private class Node{
- private int data;
- private Node next,prev;
-}
+  private class Node{
+    private int data;
+    private Node next,prev;
+  }
 
- private int size;
- private Node start,end;
+  private int size;
+  private Node start,end;
 
- public int size();
- public boolean add(int value);
- public String toString();
+  public int size() {
+    return size;
+  }
+
+  public boolean add(int value) {
+
+  }
+
+  public String toString() {
+
+  }
 }
