a68f255e9c342e1c396ba2c9fd8581bdac4a12e1
Kayla Fang <kfang00@stuy.edu>
2018-12-13 14:44:45 -0500

    added basic structure for linklist and node

---
+++ MyLinkedList.java
@@ -0,0 +1,13 @@
+public class MyLinkedList{
+ private class Node{
+ private int data;
+ private Node next,prev;
+}
+
+ private int size;
+ private Node start,end;
+
+ public int size();
+ public boolean add(int value);
+ public String toString();
+}
