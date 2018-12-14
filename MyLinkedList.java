93dd1c8b83c3c5244e128b48915b7d4a94fa3395
Kayla Fang <kfang00@stuy.edu>
2018-12-14 05:20:06 -0500

    laid out all the methods I should have

--- MyLinkedList.java
+++ MyLinkedList.java
@@ -1,21 +1,42 @@
 public class MyLinkedList{
   private class Node{
-    private int data;
+    private Integer data;
     private Node next,prev;
+
+    Node next()
+    Node prev()
+    void setNext(Node other)
+    void setPrev(Node other)
+    Integer getData()
+    Integer setData(Integer i)
+    String toString()
   }
 
-  private int size;
+  private int length;
   private Node start,end;
 
+  MyLinkedList() 
+
   public int size() {
-    return size;
+    return length;
   }
 
-  public boolean add(int value) {
+  public boolean add(Integer value) {
 
   }
 
   public String toString() {
 
   }
+
+  
+  Integer get(int index)  
+  Integer set(int index,Integer value)
+
+  boolean contains(Integer value)
+  int indexOf(Integer value)
+
+  void add(int index,Integer value) 
+  Integer remove(int index) 
+  boolean remove(Integer value) //indexOf() would also be useful
 }
